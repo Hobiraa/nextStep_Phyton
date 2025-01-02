1699fedecf72c1ff7dc4035b8d5135b3125a4f56
Mamata <hobira@gmail.com>
2025-01-02 10:32:18 +0545

    test file

---
+++ List.py
@@ -0,0 +1,37 @@
+empty_list= []
+
+
+
+list_fruits= ["Apple", "Banana", "Mango"]
+
+list_marks = [20, 30, 28]
+
+mixed_list = ["Pie" , True, 12]
+
+#changing
+print(list_fruits)
+list_fruits[0]="kiwi"
+
+print(list_fruits)
+
+list_fruits.append("Cherry")
+
+
+print(list_fruits)
+list_fruits.insert(1, "guava")
+
+print(list_fruits)
+list_fruits.remove("Mango")
+
+
+#negative_index
+print(list_fruits[-1])
+
+
+list_fruits = ["Apple", "jackfruit", "Banana", "orange"]
+
+list_fruits.reverse
+print(list_fruits)
+
+index_of_apple = list_fruits.index('Banana')
+print(index_of_apple)
